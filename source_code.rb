75af6af4c22bec83285bc70e6031ebe3d9ed26e3
Amy Jordan <amy_jordan@live.co.uk>
2019-03-02 13:59:09 +0000

    Step 14 exercise: file to read own source code and print to screen

---
+++ source_code.rb
@@ -0,0 +1,6 @@
+puts "Here is my source code"
+puts 1 + 2
+
+# This is one way to read the source code of the file you are in
+puts File.read(__FILE__)
+# __FILE__ references the name of the file you are in.
